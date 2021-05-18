53843664c584c641b382389cbaabf4d231825582
etudiant <maxime.spyropoulos@etu.upmc.fr>
2021-05-18 14:53:25 +0200

    nouveau Makefile recursif

--- Makefile
+++ Makefile
@@ -1,33 +1,22 @@
-CC = gcc
-MPI = mpicc
-CCFLAGS = -O3 -fopenmp -Wall -Wextra
-SRC_OMP = ./src/exact_cover_omp.c
-OBJ_OMP = $(SRC_OMP:.c=.o)
-SRC_SEQ = ./src/exact_cover_originel.c
-OBJ_SEQ = $(SRC_SEQ:.c=.o)
-SRC_MPI = ./src/exact_cover_mpi.c
-OBJ_MPI = $(SRC_MPI:.c=.o)
-RM = rm -f
 
 all : exact_cover_omp exact_cover_seq exact_cover_mpi
 
 test :
 	./launch_tests.sh
 
-exact_cover_omp : $(OBJ_OMP)
-	$(CC) $(CCFLAGS) -o $@ $^
+exact_cover_omp :
+	make -C ./src/ver_OMP
 
-exact_cover_seq : $(OBJ_SEQ)
-	$(CC) $(CCFLAGS) -o $@ $^
+exact_cover_seq :
+	make -C ./src/ver_seq
 
-exact_cover_mpi : $(OBJ_MPI)
-	$(MPI) $(CCFLAGS) -o $@ $^
-
-%.o: %.c
-	$(CC) $(CCFLAGS) -c $^ -o $@
+exact_cover_mpi :
+	make -C ./src/ver_MPI
 
 clean :
-	$(RM) $(OBJ_SEQ) $(OBJ_OMP) $(OBJ_MPI) exact_cover_mpi exact_cover_omp exact_cover_seq
+	make clean -C ./src/ver_OMP
+	make clean -C ./src/ver_MPI
+	make clean -C ./src/ver_seq
 
 .PHONY : clean all
 
--- launch_tests.sh
+++ launch_tests.sh
@@ -4,33 +4,33 @@ make -s #silencieux
 
 
 #OMP
-./exact_cover_omp --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_omp.txt
-./exact_cover_omp --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_omp.txt
-./exact_cover_omp --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_omp.txt
 
 
-./exact_cover_omp --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_omp.txt
-./exact_cover_omp --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_omp.txt
-./exact_cover_omp --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_omp.txt
+./src/ver_OMP/exact_cover_omp --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_omp.txt
 
 #matching 8 9 10
 
 #pento
 
 #SEQ
-./exact_cover_seq --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_seq.txt
-./exact_cover_seq --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_seq.txt
-./exact_cover_seq --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_seq.txt
 
-./exact_cover_seq --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_seq.txt
-./exact_cover_seq --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_seq.txt
-./exact_cover_seq --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_seq.txt
+./src/ver_seq/exact_cover_seq --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_seq.txt
 
 #MPI
-./exact_cover_mpi --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_mpi.txt
-./exact_cover_mpi --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_mpi.txt
-./exact_cover_mpi --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/bell12.ec -- progress-report 0 > ./resultats/bell12_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/bell13.ec -- progress-report 0 > ./resultats/bell13_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/bell14.ec -- progress-report 0 > ./resultats/bell14_mpi.txt
 
-./exact_cover_mpi --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_mpi.txt
-./exact_cover_mpi --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_mpi.txt
-./exact_cover_mpi --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/matching8.ec -- progress-report 0 > ./resultats/matching8_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/matching9.ec -- progress-report 0 > ./resultats/matching9_mpi.txt
+./src/ver_MPI/exact_cover_mpi --in ./instances/matching10.ec -- progress-report 0 > ./resultats/matching10_mpi.txt
