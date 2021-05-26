930a0af335d22f1e04afe1a9ee64a493c1eef99b
Maximilien Nowak <mnowak@frontend-buster>
2021-05-26 12:54:06 +0200

    update mpi test script

--- mpi_test.sh
+++ mpi_test.sh
@@ -1,5 +1,22 @@
 #OMP
 max=8
+rm -r resultats/MPI
+mkdir resultats/MPI
+
+for ((i=1; i <= $max; i++))
+do
+    touch ./resultats/MPI/bell12_mpi_$i.txt
+    touch ./resultats/MPI/bell13_mpi_$i.txt
+    touch ./resultats/MPI/bell14_mpi_$i.txt
+
+
+    touch ./resultats/MPI/matching8_omp_$i.txt
+    touch ./resultats/MPI/matching9_omp_$i.txt
+    touch ./resultats/MPI/matching10_omp_$i.txt
+done
+
+
+
 for ((i=1; i <= $max; i++))
 do
     mpiexec --hostfile $OAR_NODEFILE -N $i ./src/ver_MPI/exact_cover_mpi --in ./instances/bell12.ec --progress-report 0 > ./resultats/MPI/bell12_mpi_$i.txt
